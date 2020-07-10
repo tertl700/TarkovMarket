845e1f18f5561459bbd0bf578d797b3ea87371b0
Jomcgi <58116265+Jomcgi@users.noreply.github.com>
2020-07-10 03:12:32 +0100

    Updated script to work for all items
    Used OpenCV template matching to find text and then tesseract OCR to retrieve text

---
+++ src/item_id.py
@@ -0,0 +1,29 @@
+import item_price
+from PIL import Image
+from pynput import keyboard
+from mss import mss
+
+# on pg_up, get item_id and check item_price. on pg_down exit script
+def on_press(key):
+
+    if key == keyboard.Key.page_up:
+        screenshot()
+        item_price.main()
+    elif key == keyboard.Key.page_down:
+        item_price.driver.quit()
+        exit()
+
+# screenshot main monitor using mss, convert to PIL image for processing
+def screenshot():   
+    with mss() as sct:
+        monitor = sct.monitors[1]
+        sct_img = sct.grab(monitor)
+        # Convert to PIL/Pillow Image
+        img = Image.frombytes('RGB', sct_img.size, sct_img.bgra, 'raw', 'BGRX')
+        img.save('images/screenshot.png')
+
+
+# Collect events until released
+with keyboard.Listener(
+        on_press=on_press) as listener:
+    listener.join()
---
+++ src/item_price.py
@@ -0,0 +1,52 @@
+from selenium import webdriver
+import img_processing
+import time
+import os
+
+
+# make directory for storing images if it does not exist in path
+if not os.path.exists('images'):
+    os.makedirs('images')
+
+# hide mozilla
+os.environ['MOZ_HEADLESS'] = '1'
+
+# start selenium driver for firefox and open main page of tarkov-market
+driver = webdriver.Firefox()
+driver.get("https://tarkov-market.com")
+print('Ready')
+
+# find search bar and enter text from img_processing
+def main():
+    global driver
+    
+    search_bar = driver.find_element_by_css_selector("input[placeholder='Search'")
+    search_bar.clear()
+    search_bar.send_keys(img_processing.run())
+    time.sleep(2.5)
+    
+    try:
+        item_price = driver.find_element_by_class_name('price-main')
+        print(f'Flea: {item_price.text}')
+        result = True
+
+    except:
+        print('No price detected')
+        print('logged broken crop')
+        result = False
+
+    try:
+        slot_price = driver.find_element_by_class_name('price-sec')
+        print(f'Per Slot: {slot_price.text}')
+
+    except:
+        try:
+            print(f'Per Slot: {item_price.text}')
+        except Exception:
+            pass
+
+
+    # trader = driver.find_element_by_class_name('item-card')
+    # print(trader)
+
+    img_processing.log_image(result)
