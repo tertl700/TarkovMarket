df3c91db0a1eace7699e6a40a4318faea6f66b09
Colin Cheung <ColinCee@users.noreply.github.com>
2020-07-14 21:17:19 +0100

    Fix crash when item not found

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 import asyncio
 import keyboard
 from src import globals, screenshot, item_price
+
+HOTKEY = "CTRL+Z"
+
 async def main():
     try:
+        print("Starting puppeteer...")
         await globals.init()
         # Collect events until released
-        print ("Running...")
+        print ("Inspect an item and press CTRL+Z to start")
         while True:
-            keyboard.wait('ctrl+\\')
+            keyboard.wait(HOTKEY)
             screenshot.capture()
             await item_price.fetch_price()
     except Exception as e:
--- src/item_price.py
+++ src/item_price.py
@@ -1,7 +1,7 @@
 import src.globals as globals
 from src.img_processing import run
 import os
-
+from pyppeteer.errors import TimeoutError
 # make directory for storing images if it does not exist in path
 if not os.path.exists("images"):
     os.makedirs("images")
@@ -11,18 +11,28 @@ async def fetch_price():
     page = globals.page
     search_bar = await page.querySelector('.search input[type="text"]')
     item_name = run()
-    print(f"item name: {item_name}")
 
     await page.evaluate("""
     () => { const input = document.querySelector('.search input[type="text"]');
     input.value = '';
     }""")
     await search_bar.type(item_name)
-    await page.waitForXPath(f"//span[contains(text(),'{item_name}')]")
+    if not await does_item_exist(page, item_name):
+        print(f"Could not find item: {item_name}")
+        return
+    
     main_price = await get_text_from_selector("span.price-main")
     price_per_slot = await get_text_from_selector("span.price-sec")
+    print(f"Item name: {item_name}")
     print(f"Price is: {main_price}")
     print(f"Price per slot: {price_per_slot}")
+
+async def does_item_exist(page, item_name):
+    try:
+        await page.waitForXPath(f"//span[contains(text(),'{item_name}')]", options={"timeout": 3000} )
+        return True
+    except TimeoutError:
+        return False
 
 async def get_text_from_selector(selector):
     page = globals.page
@@ -35,8 +45,3 @@ async def get_text_from_selector(selector):
     )
 
     return text.strip(' \n\r')
-
-# trader = driver.find_element_by_class_name('item-card')
-# print(trader)
-
-# img_processing.log_image(result)
